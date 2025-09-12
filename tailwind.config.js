234a865131afcc77cd6cccd18e3a72d24f17628a
Eli H <elih@bezy.org>
2025-09-11 20:06:00 -0700

    Configure Astro with GitHub Pages deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.{html,js}"],
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
